4b3d0a86418187117241e1345973ea710358d3f3
tuanlnapk00690 <tuanlnapk00690@gmail.com>
2016-09-20 10:53:32 +0700

    Lab01
    About me

--- gen/com/giaodienandroid/R.java
+++ gen/com/giaodienandroid/R.java
@@ -22,37 +22,52 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int avatar=0x7f020000;
+        public static final int b=0x7f020001;
+        public static final int c=0x7f020002;
+        public static final int d=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int icon=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
-        public static final int btnLab01=0x7f080006;
-        public static final int btnLab02=0x7f080007;
-        public static final int btnLab03=0x7f080008;
-        public static final int btnLab04=0x7f080009;
-        public static final int btnLab05=0x7f08000a;
-        public static final int btnLab06=0x7f08000b;
-        public static final int btnLab07=0x7f08000c;
-        public static final int btnLab08=0x7f08000d;
-        public static final int tableRow1=0x7f080001;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080003;
-        public static final int textView4=0x7f080004;
-        public static final int textView5=0x7f080005;
+        public static final int action_settings=0x7f080015;
+        public static final int btnLab01=0x7f08000d;
+        public static final int btnLab02=0x7f08000e;
+        public static final int btnLab03=0x7f08000f;
+        public static final int btnLab04=0x7f080010;
+        public static final int btnLab05=0x7f080011;
+        public static final int btnLab06=0x7f080012;
+        public static final int btnLab07=0x7f080013;
+        public static final int btnLab08=0x7f080014;
+        public static final int imageView1=0x7f080001;
+        public static final int imgbtnAboutme=0x7f08000c;
+        public static final int scrollView1=0x7f080000;
+        public static final int tableRow1=0x7f08000b;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080004;
+        public static final int textView4=0x7f080005;
+        public static final int textView5=0x7f080006;
+        public static final int textView6=0x7f080007;
+        public static final int textView7=0x7f080008;
+        public static final int textView8=0x7f080009;
+        public static final int textView9=0x7f08000a;
     }
     public static final class layout {
-        public static final int activity_lab04=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_aboutme=0x7f030000;
+        public static final int activity_lab04=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int lab03=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int aboutme_=0x7f070000;
+        public static final int lab03=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_aboutme_=0x7f050004;
         public static final int title_activity_lab03=0x7f050003;
     }
     public static final class style {
--- src/com/giaodienandroid/MainActivity.java
+++ src/com/giaodienandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.giaodienandroid;
 
 import com.Lab04.Lab04Activity;
+import com.aboutme.Aboutme_Activity;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,14 +10,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
 	Button btnLab04;
+	ImageButton imgbtnAboutme;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		imgbtnAboutme = (ImageButton) findViewById(R.id.imgbtnAboutme);
+		imgbtnAboutme.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this,Aboutme_Activity.class);
+				startActivity(intent);
+			}
+		});
 		
 		btnLab04 = (Button) findViewById(R.id.btnLab04);
 		btnLab04.setOnClickListener(new View.OnClickListener() {
